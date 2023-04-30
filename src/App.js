007ee574df051d1274d49efff8490be77c82906c
Dinuka Dissanayake <it21024818@my.sliit.lk>
2023-04-30 19:21:22 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import Vehicles from "./components/Vehicles";
 import Filter from "./components/Filter";
@@ -6,27 +6,25 @@ import Basket from "./components/Basket";
 import store from "./store";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="container">
-          <h1>Vehicle Bidding Application</h1>
-          <hr />
-          <div className="row">
-            <div className="col-md-9">
-              <Filter />
-              <hr />
-              <Vehicles />
-            </div>
-            <div className="col-md-3">
-              <Basket />
-            </div>
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div className="container">
+        <h1>Vehicle Bidding Application</h1>
+        <hr />
+        <div className="row">
+          <div className="col-md-9">
+            <Filter />
+            <hr />
+            <Vehicles />
+          </div>
+          <div className="col-md-3">
+            <Basket />
           </div>
         </div>
-      </Provider>
-    );
-  }
-}
+      </div>
+    </Provider>
+  );
+};
 
 export default App;
--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { addToBid } from "../actions/bidActions";
 import { fetchVehicles } from "../actions/vehiclesActions";
@@ -8,25 +8,49 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { TextField } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
+import Snackbar from "@material-ui/core/Snackbar";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const Vehicles = ({ vehicles, bidItems, fetchVehicles, addToBid, handleAddToBid }) => {
   useEffect(() => {
     fetchVehicles();
   }, [fetchVehicles]);
 
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    const onlyNums = value.replace(/[^0-9]/g, "");
+    setInputValue(onlyNums);
+  };
+
+  const handleSnackbarClose = () => {
+    setError(null);
+  };
+
+  const handleAddToBidWithValidation = (e, vehicle) => {
+    e.preventDefault();
+    const minValue = vehicle.details.price;
+    if (parseInt(inputValue) < minValue) {
+      setError(`Bid amount should be greater than or equal to LKR ${minValue}`);
+    } else {
+      handleAddToBid(e, vehicle);
+      setInputValue("");
+    }
+  };
+
   const vehicleItems = vehicles.map((vehicle) => (
     <div className="col-md-4" key={vehicle.id}>
       <Card>
-        <CardMedia
-          component="img"
-          alt={vehicle.name}
-          height="200"
-          image={vehicle.details.image}
-          name={vehicle.name}
-        />
+        <CardMedia component="img" alt={vehicle.name} height="200" image={vehicle.details.image} name={vehicle.name} />
         <CardContent>
           <Typography gutterBottom variant="h4" component="h2">
-            {vehicle.name}
+            {vehicle.name} {vehicle.details.brand} {vehicle.details.manufactureYear}
           </Typography>
           <Typography gutterBottom variant="h6" component="h2">
             {vehicle.details.description}
@@ -34,18 +58,17 @@ const Vehicles = ({ vehicles, bidItems, fetchVehicles, addToBid, handleAddToBid 
           {/* <b>{util.formatCurrency(vehicle.price)}</b> */}
           <div>
             <div>
-              <TextField required></TextField>
+              <TextField
+                required
+                value={inputValue}
+                onChange={handleInputChange}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+              />
             </div>
-            <div>
-              LKR
-            </div>
+            <div>LKR</div>
           </div>
 
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={(e) => handleAddToBid(e, vehicle)}
-          >
+          <Button variant="contained" color="primary" onClick={(e) => handleAddToBidWithValidation(e, vehicle)} disabled={!inputValue.trim()}>
             submit
           </Button>
         </CardContent>
@@ -53,7 +76,16 @@ const Vehicles = ({ vehicles, bidItems, fetchVehicles, addToBid, handleAddToBid 
     </div>
   ));
 
-  return <div className="row">{vehicleItems}</div>;
+  return (
+    <div className="row">
+      {vehicleItems}
+      <Snackbar open={error !== null} autoHideDuration={3000} onClose={handleSnackbarClose}>
+        <Alert onClose={handleSnackbarClose} severity="error">
+          {error}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => ({
